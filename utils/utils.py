1053c89b4c7f5eebc1f37488f2fe5725b5445c07
RumbleJack56 <ujjwalkakar2005@gmail.com>
2024-11-20 23:33:57 +0530

    pix to pix working

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from torchvision.utils import make_grid
+
+class Interpolate(nn.Module):
+    def __init__(self, scale_factor, mode):
+        super(Interpolate, self).__init__()
+        self.interp = nn.functional.interpolate
+        self.scale_factor = scale_factor
+        self.mode = mode
+        
+    def forward(self, x):
+        x = self.interp(x, scale_factor=self.scale_factor, mode=self.mode, align_corners=True)
+
+        return x
